5cc9ef4b559a2dfa325867547b2dbe4ee0aba6e8
Naftali Kulik <nkulik1994@gmail.com>
2022-05-30 17:08:23 -0400

    completed first deliverable

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plantList, updateList] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+      .then(r => r.json())
+      .then(plants => updateList(plants))
+  }, [])
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plants={plantList} />
     </main>
   );
 }
